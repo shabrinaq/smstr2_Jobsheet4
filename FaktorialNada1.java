1f838ad2044f195a4c0d62db53a09122ac422dfe
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-03-18 21:05:14 +0700

    first commit

--- FaktorialNada1.java
+++ FaktorialNada1.java
@@ -13,6 +13,16 @@ public class FaktorialNada1 {
         return fakto;
     }
 
+    public int faktorialBFWhile(int n) {
+        int fakto = 1;
+        int i = 1;
+        while (i <= n) {
+            fakto = fakto * 1;
+            i++;
+        }
+        return fakto;
+    }
+
     public int faktorialDC(int n) {
         if (n == 1) {
             return 1;
